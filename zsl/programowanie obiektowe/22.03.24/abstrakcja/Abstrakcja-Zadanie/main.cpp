9c2faf1c2a58d12dfea47c087adbba6f6299bb17
JedraJacek <89982768+JedraJacek@users.noreply.github.com>
2022-04-20 20:39:47 +0200

    Add files via upload

---
+++ zsl/programowanie obiektowe/22.03.24/abstrakcja/Abstrakcja-Zadanie/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Person{
+public:
+    string name;
+    string surname;
+
+    virtual void showData()=0;
+};
+
+class Worker : public Person {
+public:
+    string position;
+
+    void showData(){
+        cout << "Imiê i nazwisko: " << name + " " + surname << ", pozycja: " << position << endl;
+    }
+};
+
+class Student : public Person {
+public:
+    string classAtSchool;
+
+    void showData(){
+        cout << "Imiê i nazwisko: " << name + " " + surname << ", klasa: " << classAtSchool << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "polish");
+
+    //Person osoba;
+
+    Worker pracownik1;
+    pracownik1.name="Janusz";
+    pracownik1.surname="Nowak";
+    pracownik1.position="Programista";
+    cout << "Dane pracownika:\n";
+    pracownik1.showData();
+
+    Student uczen1;
+    uczen1.name="Anna";
+    uczen1.surname="Kowalska";
+    uczen1.classAtSchool="2PT";
+    cout << "\nDane Ucznia:\n";
+    uczen1.showData();
+
+    return 0;
+}
